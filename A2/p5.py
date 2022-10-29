6f1cf259a95a90ba4f86a723933fe8f9e9942b14
Wu Jiaxin <lisa24@connect.hku.hk>
2022-10-29 16:09:33 +0800

    A2 p5,6

--- A2/p5.py
+++ A2/p5.py
@@ -5,17 +5,21 @@ def min_max_mulitple_ghosts(problem, k):
     #Your p5 code here
     solution = parse.Solution(problem.seed, \
         [parse.State(None, None, copy.deepcopy(problem.layout), 0)], None)
+    pacman_actions = [] # a list to record all the past actions of the pacman
     pacTurn = True
     while (problem.pacWin()==False and problem.pacLose()==False):
         if pacTurn == True:
-            _, best_di = max_agent(problem, k)
+            _, best_di = max_agent(problem, (1+len(problem.ghostlocs))*k) # (1+len(problem.ghostlocs))*k actions in total
+            pacman_actions.append(best_di)
             problem.move("P", best_di)
             state = parse.State("P", best_di, copy.deepcopy(problem.layout), problem.score)
+            solution.statelist.append(state)
+            # print(state.printLayout())
             if problem.pacWin() == True or problem.pacLose() == True: break
             pacTurn = False
         else:
             for ghost in problem.ghostlocs.keys():
-                _, best_di = min_agent(problem, k, ghost)
+                _, best_di = min_agent(problem, (1+len(problem.ghostlocs))*k, ghost)
                 if best_di != '':
                     problem.move(ghost, best_di)
                 state = parse.State(ghost, best_di, copy.deepcopy(problem.layout), problem.score)
@@ -30,13 +34,14 @@ def max_agent(problem_state: parse.Problem, depth):
     if problem_state.pacWin() or problem_state.pacLose():
         return problem_state.score, ''
     if depth == 0:
-        return eval_func(problem_state, 'W'), ''
+        return eval_func(problem_state, 'P'), ''
     if depth > 0:
         feasible_direction = problem_state.feasibleDirection("P")
         if len(feasible_direction) == 1:
             next_problem_state = copy.deepcopy(problem_state)
             next_problem_state.move("P", feasible_direction[0])
-            return next_problem_state.score, feasible_direction[0]
+            score, _ = min_agent(next_problem_state, depth-1, 'W')
+            return score, feasible_direction[0]
         v = -1e9
         for di in feasible_direction:
             next_problem_state = copy.deepcopy(problem_state)
@@ -46,7 +51,7 @@ def max_agent(problem_state: parse.Problem, depth):
                 v = score
                 best_di = di
             elif score == v:
-                best_di = random.choice((di, best_di)) # avoid infinte loop
+                best_di = random.choice((di, best_di))
         return v, best_di
 
 # ghost agent
@@ -54,19 +59,19 @@ def min_agent(problem_state: parse.Problem, depth, ghost):
     if problem_state.pacWin() or problem_state.pacLose():
         return problem_state.score, ''
     if depth == 0:
-        if ghost == list(problem_state.ghostlocs.keys())[-1]:
-            return eval_func(problem_state, 'P'), ''
-        else:
-            return eval_func(problem_state, chr(ord(ghost)+1)), ''
+        return eval_func(problem_state, ghost), ''
     if depth > 0:
         feasible_direction = problem_state.feasibleDirection(ghost)
-        if  feasible_direction == ():
-            return problem_state.score, ''
-        if len(feasible_direction) == 1:
-            next_problem_state = copy.deepcopy(problem_state)
-            next_problem_state.move(ghost, feasible_direction[0])
-            return next_problem_state.score, feasible_direction[0]
+        # if this is the last ghost, next player is max_agent
         if ghost == list(problem_state.ghostlocs.keys())[-1]:
+            if  feasible_direction == ():
+                score, _ = max_agent(problem_state, depth-1)
+                return score, ''
+            if len(feasible_direction) == 1:
+                next_problem_state = copy.deepcopy(problem_state)
+                next_problem_state.move(ghost, feasible_direction[0])
+                score, _ = max_agent(next_problem_state, depth-1)
+                return score, feasible_direction[0]
             v = 1e9
             for di in problem_state.feasibleDirection(ghost):
                 next_problem_state = copy.deepcopy(problem_state)
@@ -76,8 +81,16 @@ def min_agent(problem_state: parse.Problem, depth, ghost):
                     v = score
                     best_di = di
                 elif score == v:
-                    best_di = random.choice((di, best_di)) # avoid infinte loop
+                    best_di = random.choice((di, best_di))
         else:
+            if feasible_direction == ():
+                score, _ = min_agent(problem_state, depth-1, chr(ord(ghost)+1))
+                return score, ''
+            if len(feasible_direction) == 1:
+                next_problem_state = copy.deepcopy(problem_state)
+                next_problem_state.move(ghost, feasible_direction[0])
+                score, _ = min_agent(next_problem_state, depth-1, chr(ord(ghost)+1))
+                return score, feasible_direction[0]
             v = 1e9
             for di in feasible_direction:
                 next_problem_state = copy.deepcopy(problem_state)
@@ -87,30 +100,29 @@ def min_agent(problem_state: parse.Problem, depth, ghost):
                     v = score
                     best_di = di
                 elif score == v:
-                    best_di = random.choice((di, best_di)) # avoid infinte loop
+                    best_di = random.choice((di, best_di))
         return v, best_di
 
 def eval_func(problem_state: parse.Problem, next_player):
-    dist_to_closest_ghost = 100000
     dist_to_closest_food = 100000
     ghost_score = 0
     food_score = 0
     for ghost in problem_state.ghostlocs.keys():
         dist = abs(problem_state.pacmanloc[0]-problem_state.ghostlocs[ghost][0]) + abs(problem_state.pacmanloc[1]-problem_state.ghostlocs[ghost][1])
-        if dist == 1 and ghost == next_player:
-            return problem_state.score+parse.PACMAN_EATEN_SCORE
-        if dist < dist_to_closest_ghost:
-            dist_to_closest_ghost = dist
+        if dist == 1 and 'P' != next_player:
+            return problem_state.score + parse.PACMAN_EATEN_SCORE
+        # larger penalty if distance to a ghost is less than 4
         if dist < 4:
-            ghost_score += parse.PACMAN_EATEN_SCORE/(dist+1)**2
-        ghost_score += dist_to_closest_ghost/2
+            ghost_score -= 5/dist
+        else:
+            ghost_score -= 1/dist
     for food_loc in problem.foodlocs:
         dist = abs(problem_state.pacmanloc[0]-food_loc[0]) + abs(problem_state.pacmanloc[1]-food_loc[1])
         if dist < dist_to_closest_food:
             dist_to_closest_food = dist
         if dist_to_closest_food <= 1 and len(problem_state.foodlocs)==1 and next_player=="P":
             return problem_state.score + parse.PACMAN_WIN_SCORE
-        food_score += 10-dist_to_closest_food
+    food_score += 10-dist_to_closest_food
     return problem_state.score + ghost_score + food_score + parse.PACMAN_MOVING_SCORE
 
 if __name__ == "__main__":
--- A2/p6.py
+++ A2/p6.py
@@ -1,11 +1,127 @@
+from random import random
 import sys, parse
-import time, os, copy
+import time, os, copy, random
 
 def expecti_max_mulitple_ghosts(problem, k):
     #Your p6 code here
-    solution = ''
-    winner = ''
-    return solution, winner
+    solution = parse.Solution(problem.seed, \
+        [parse.State(None, None, copy.deepcopy(problem.layout), 0)], None)
+    pacTurn = True
+    while (problem.pacWin()==False and problem.pacLose()==False):
+        if pacTurn == True:
+            feasible_direction = problem.feasibleDirection("P")
+            if len(feasible_direction) == 1:
+                best_direction = feasible_direction[0]
+            else:
+                _, best_direction = expectimax(problem, k)
+            problem.move("P", best_direction)
+            state = parse.State("P", best_direction, copy.deepcopy(problem.layout), problem.score)
+            solution.statelist.append(state)
+            # print(state.printLayout())
+            pacTurn = False
+        else:
+            for ghost in problem.ghostlocs.keys():
+                feasible_direction = problem.feasibleDirection(ghost)
+                if feasible_direction==():
+                    state = parse.State(ghost, '', copy.deepcopy(problem.layout), problem.score)
+                    solution.statelist.append(state)
+                else:
+                    rand_di = random.choice(feasible_direction)
+                    problem.move(ghost, rand_di)
+                    state = parse.State(ghost, rand_di, copy.deepcopy(problem.layout), problem.score)
+                    solution.statelist.append(state)
+                    if problem.pacLose(): break
+            pacTurn = True
+    solution.winner = "Pacman" if problem.pacWin() else "Ghost"
+    return solution.output(), solution.winner
+
+def expectimax(problem_state: parse.Problem, depth):
+    if problem_state.pacWin() or problem_state.pacLose():
+        return problem_state.score, ''
+    if depth == 0:
+        return eval_func(problem_state), ''
+    if depth > 0:
+        feasible_direction = problem_state.feasibleDirection("P")
+        if len(feasible_direction) == 1:
+            next_problem_state = copy.deepcopy(problem_state)
+            next_problem_state.move("P", feasible_direction[0])
+            score = ghost_agent(next_problem_state, depth, 'W')
+            return score, feasible_direction[0]
+        else:
+            best_average_score = -10000
+            for di in feasible_direction:
+                next_problem_state = copy.deepcopy(problem_state)
+                next_problem_state.move('P', di)
+                average_score = ghost_agent(next_problem_state, depth, 'W')
+                if average_score >  best_average_score:
+                    best_average_score = average_score
+                    best_direction = di
+                if average_score == best_average_score:
+                    best_direction = random.choice([best_direction, di])
+            return best_average_score, best_direction
+
+def ghost_agent(problem_state: parse.Problem, depth, ghost) -> int:
+    if problem_state.pacWin() or problem_state.pacLose():
+        return problem_state.score
+    feasible_direction = problem_state.feasibleDirection(ghost)
+    # if this is the last ghost
+    if ghost == list(problem_state.ghostlocs.keys())[-1]:
+        if feasible_direction == ():
+            score, _ = expectimax(problem_state, depth-1)
+            return score
+        if len(feasible_direction) == 1:
+            next_problem_state = copy.deepcopy(problem_state)
+            next_problem_state.move(ghost, feasible_direction[0])
+            score, _ = expectimax(next_problem_state, depth-1)
+            return score
+        else:
+            score_sum = 0
+            for di in feasible_direction:
+                next_problem_state = copy.deepcopy(problem_state)
+                next_problem_state.move(ghost, di)
+                score, _ = expectimax(next_problem_state, depth-1)
+                score_sum += score
+            return score_sum / len(feasible_direction)
+    else:
+        if feasible_direction == ():
+            score = ghost_agent(problem_state, depth, chr(ord(ghost)+1))
+            return score
+        if len(feasible_direction) == 1:
+            next_problem_state = copy.deepcopy(problem_state)
+            next_problem_state.move(ghost, feasible_direction[0])
+            score = ghost_agent(next_problem_state, depth, chr(ord(ghost)+1))
+            return score
+        else:
+            score_sum = 0
+            for di in feasible_direction:
+                next_problem_state = copy.deepcopy(problem_state)
+                next_problem_state.move(ghost, di)
+                score = ghost_agent(next_problem_state, depth, chr(ord(ghost)+1))
+                score_sum += score
+            return score_sum / len(feasible_direction)
+
+
+def eval_func(problem_state: parse.Problem):
+    dist_to_closest_food = 100000
+    ghost_score = 0
+    food_score = 0
+    for ghost in problem_state.ghostlocs.keys():
+        dist = abs(problem_state.pacmanloc[0]-problem_state.ghostlocs[ghost][0]) + abs(problem_state.pacmanloc[1]-problem_state.ghostlocs[ghost][1])
+        if dist == 0:
+            return problem_state.score + parse.PACMAN_EATEN_SCORE
+        # penalty for distance to ghost
+        # Since the next player is pacman, it is likely to escape when dist>=1, so the penalty is not too high
+        ghost_score -= 1/dist
+
+    for food_loc in problem_state.foodlocs:
+        dist = abs(problem_state.pacmanloc[0]-food_loc[0]) + abs(problem_state.pacmanloc[1]-food_loc[1])
+        if dist < dist_to_closest_food:
+            dist_to_closest_food = dist
+        # if pacman can win in the next action
+        if dist_to_closest_food <= 1 and len(problem_state.foodlocs)==1:
+            return problem_state.score + parse.PACMAN_WIN_SCORE
+    food_score += 10 - dist_to_closest_food
+    return problem_state.score + ghost_score + food_score + parse.PACMAN_MOVING_SCORE
 
 if __name__ == "__main__":
     test_case_id = int(sys.argv[1])    
