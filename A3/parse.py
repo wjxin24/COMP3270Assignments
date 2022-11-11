2fc59293a4b830dbc97814fed9d0c6010de6ad17
Wu Jiaxin <lisa24@connect.hku.hk>
2022-11-12 00:26:53 +0800

    A3 p1

--- A3/parse.py
+++ A3/parse.py
@@ -1,6 +1,50 @@
+import random, p1
+
+ENSW = {'E': (0,1), 'N': (-1,0), 'S': (1,0), 'W': (0,-1)}
+NOISE_DI = {'N':['N', 'E', 'W'], 'E':['E', 'S', 'N'], 'S':['S', 'W', 'E'], 'W':['W', 'N', 'S']}
+
+
+            
+
 def read_grid_mdp_problem_p1(file_path):
     #Your p1 code here
-    problem = ''
+    with open(file_path, 'r') as file:
+        line = file.readline()
+        assert line.startswith("seed:")
+        seed = int(line.split()[1])
+        line = file.readline()
+        assert line.startswith("noise:")
+        noise = float(line.split()[1])
+        line = file.readline()
+        assert line.startswith("livingReward:")
+        livingReward = float(line.split()[1])
+        line = file.readline()
+        
+        assert line.startswith("grid:")
+        layout = []
+        row = 0
+        line = file.readline()
+        player = []
+        while line.startswith("policy:") == False:
+            row_list = line.split()
+            for col in range(len(row_list)):
+                if row_list[col] == 'S':
+                    player = [row, col]
+            row += 1
+            layout.append(row_list)
+            line = file.readline()
+        
+        assert line.startswith("policy:")
+        policy = []
+        row = 0
+        line = file.readline()
+        while line:
+            row_list = line.split()
+            row += 1
+            policy.append(row_list)
+            line = file.readline()
+    state = p1.State(layout, player, policy)
+    problem = p1.Problem(seed, noise, livingReward, state, policy)
     return problem
 
 def read_grid_mdp_problem_p2(file_path):
