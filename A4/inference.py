95740e645df2f64ad743a88c682b0c8392473980
Wu Jiaxin <lisa24@connect.hku.hk>
2022-12-04 12:50:15 +0800

    A4 Q7

--- A4/inference.py
+++ A4/inference.py
@@ -389,7 +389,12 @@ class ParticleFilter(InferenceModule):
         gameState.
         """
         "*** YOUR CODE HERE ***"
-        raiseNotDefined()
+        newParticles = []
+        for oldPos in self.particles:
+            newPosDist = self.getPositionDistribution(gameState, oldPos)
+            newParticles.append(newPosDist.sample())
+        self.particles = newParticles
+        # raiseNotDefined()
 
     def getBeliefDistribution(self):
         """
